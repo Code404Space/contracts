326c60db767b320f5fe469be82348b2670a98643
Yuri Cooliq <44652788+yurycooliq@users.noreply.github.com>
2024-02-09 15:08:45 +0000

    Test updated

--- test/Code404.test.ts
+++ test/Code404.test.ts
@@ -1,13 +1,9 @@
-import {
-  time,
-  loadFixture,
-} from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Lock", function () {
-  async function deployOneYearLockFixture() {
+describe("Code404", function () {
+  async function deployCode404() {
     const [governor, user] = await ethers.getSigners();
 
     const Code404 = await ethers.getContractFactory("Code404");
@@ -18,12 +14,14 @@ describe("Lock", function () {
 
   describe("Token", function () {
     it("Should have ERC20 attributes", async function () {
-      const { code404 } = await loadFixture(deployOneYearLockFixture);
+      const { code404 } = await loadFixture(deployCode404);
 
       expect(await code404.name()).to.equal("Code 404");
       expect(await code404.symbol()).to.equal("CODE404");
       expect(await code404.decimals()).to.equal(18);
-      expect(await code404.totalSupply()).to.equal(BigInt(1e18.toString()) * 10000n);
+      expect(await code404.totalSupply()).to.equal(
+        BigInt((1e18).toString()) * 10000n
+      );
     });
   });
 });
